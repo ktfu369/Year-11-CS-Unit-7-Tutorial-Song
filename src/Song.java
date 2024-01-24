8b0e4b4049ff20c1c154985da2bedfd634e7b4bb
Katie <kf1030@student.cis.edu.hk>
2024-01-24 09:50:02 +0800

    update

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title,artist;
+    private ArrayList<String> listen = new ArrayList<>();
+    public Song(String a, String b){
+        title = a;
+        artist = b;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getArtist(){
+        return artist;
+    }
+    public int howMany(ArrayList<String> names){
+        int cnt=0;
+        for(String name:names){
+            name = name.toLowerCase();
+            int f1=0;
+            for(String lname:listen){
+                if(name.equals(lname)){
+                    f1=1;
+                    break;
+                }
+            }
+            if(f1==0){
+                cnt++;
+                listen.add(name);
+            }
+        }
+        return cnt;
+    }
 }
